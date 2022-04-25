7f8290be5a8f378943f686b8ab9ee62fb002038f
Ram <56319438+rameshdhanapal@users.noreply.github.com>
2022-04-25 14:50:12 -0400

    Add files via upload

---
+++ src/style/Home.tsx
@@ -0,0 +1,14 @@
+import { Button } from '@mui/material';
+import React, { ChangeEvent, useState } from 'react'
+import UploadButtons from '../fileupload/UploadButtons';
+
+
+function Home() {  
+ 
+
+  return (
+    <h1>Home</h1>
+  )
+}
+
+export default Home
